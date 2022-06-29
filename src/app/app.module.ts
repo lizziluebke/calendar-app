5bafbdcd8e64ccdcc3c8ec91f82b3243615d93c6
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-06-29 15:10:24 -0500

    showing a cal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CalComponent } from './cal/cal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    CalComponent
   ],
   imports: [
     BrowserModule
